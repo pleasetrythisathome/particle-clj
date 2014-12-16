f694c2dd12b3e5514275efb1e72c1edd40d8d0fb
Dylan Butman <dbutman@gmail.com>
2014-12-16 12:59:59 -0500

    basic particle system

--- src/particle_clj/sketch.clj
+++ src/particle_clj/sketch.clj
@@ -1,15 +1,45 @@
 (ns particle-clj.sketch
   (:require
-   [quil.core :as q]))
+   [plumbing.core :refer :all]
+   [schema.core :as s]
+   [quil.core :as q]
+   [thi.ng.geom.core :as g]
+   [thi.ng.geom.core.vector :as v]))
 
+(defprotocol IRender
+  (render [this]))
+
+(defrecord Particle [pos]
+  IRender
+  (render [this]
+    (apply q/point pos)))
+
+(def new-particle-schema
+  {:pos (s/pred v/vec2?)})
+
+(defn new-particle [& {:as opts}]
+  (->> opts
+       (merge {:pos (v/vec2)})
+       (s/validate new-particle-schema)
+       (map->Particle)))
+
+(defn random-particle [w h]
+  (new-particle :pos (v/vec2 (rand-int w) (rand-int h))))
 
 (defn setup
   []
-  {})
+  (let [num-particles 5000]
+    (q/frame-rate 60)
+    {:particles (for [_ (range num-particles)]
+                  (random-particle (q/width) (q/height)))}))
 
 (defn draw
   [state]
   ;;(println state)
-  (q/background 200)
-  ;;state
-  )
+  (q/background 255)
+
+  (q/fill 0)
+  (q/stroke-weight 2)
+  (doseq [p (:particles state)]
+    (render p))
+  state)
